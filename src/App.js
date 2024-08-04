1da4ac58b5b6d32dbcffbdf968d79d3315b62e4f
NikhilDongre1 <nikhildongre8109.com>
2024-08-04 22:24:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const handleButtonClick = (e) => {
+    const newValue = e.target.value;
+    const lastChar = value.slice(-1);
+
+    if (newValue === '.') {
+    
+      if (value.includes('.') && value.split(/[-+*/]/).pop().includes('.')) return;
+      setValue(value + newValue);
+    } else if (['+', '-', '*', '/'].includes(newValue)) {
+      
+      if (!['+', '-', '*', '/'].includes(lastChar)) {
+        setValue(value + newValue);
+      }
+    } else {
+      setValue(value + newValue);
+    }
+  };
+
+  const handleEqualClick = () => {
+    try {
+   
+      const result = eval(value);
+      setValue(result.toString());
+    } catch {
+      setValue('Error');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator">
+        <form action="">
+          <div className="display">
+            <input type="text" value={value} readOnly />
+          </div>
+          <div>
+            <input type="button" value="AC" onClick={() => setValue('')} />
+            <input type="button" value="DE" onClick={() => setValue(value.slice(0, -1))} />
+            <input type="button" value="." onClick={handleButtonClick} />
+            <input type="button" value="/" onClick={handleButtonClick} />
+          </div>
+          <div>
+            <input type="button" value="7" onClick={handleButtonClick} />
+            <input type="button" value="8" onClick={handleButtonClick} />
+            <input type="button" value="9" onClick={handleButtonClick} />
+            <input type="button" value="*" onClick={handleButtonClick} />
+          </div>
+          <div>
+            <input type="button" value="4" onClick={handleButtonClick} />
+            <input type="button" value="5" onClick={handleButtonClick} />
+            <input type="button" value="6" onClick={handleButtonClick} />
+            <input type="button" value="+" onClick={handleButtonClick} />
+          </div>
+          <div>
+            <input type="button" value="3" onClick={handleButtonClick} />
+            <input type="button" value="2" onClick={handleButtonClick} />
+            <input type="button" value="1" onClick={handleButtonClick} />
+            <input type="button" value="-" onClick={handleButtonClick} />
+          </div>
+          <div>
+            <input type="button" value="00" onClick={handleButtonClick} />
+            <input type="button" value="0" onClick={handleButtonClick} />
+            <input type="button" value="=" className="equal" onClick={handleEqualClick} />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
